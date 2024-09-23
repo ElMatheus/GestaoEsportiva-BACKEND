d7cf208b4f026813ac5a994d68585afc1243455b
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-09-23 11:52:12 -0300

    refactor: Update campeonato.routes.js
    This commit updates the `campeonato.routes.js` file to include the necessary routes for CRUD operations on the `Campeonato` entity. It adds routes for getting all campeonatos, getting a campeonato by ID, creating a new campeonato, updating an existing campeonato, and deleting a campeonato. These routes provide full functionality for managing campeonatos in the system.

--- src/routes/campeonato.routes.js
+++ src/routes/campeonato.routes.js
@@ -9,4 +9,15 @@ import {
     getCampeonatoByTitulo,
     getDuracaoCampeonato,
 } from "../controllers/campeonato.controller.js";
-    
+
+const campeonatoRouter = Router();
+
+campeonatoRouter.get("/", getCampeonatos);
+campeonatoRouter.get("/:id", getCampeonatoById);
+campeonatoRouter.post("/", createCampeonato);
+campeonatoRouter.put("/:id", updateCampeonato);
+campeonatoRouter.delete("/:id", deleteCampeonato);
+campeonatoRouter.get("/titulo/:titulo", getCampeonatoByTitulo);
+campeonatoRouter.get("/duracao/:id", getDuracaoCampeonato);
+
+export default campeonatoRouter;
