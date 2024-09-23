a876b8ca2c5bca5558d3d36d6827e1587a33ca93
MagoValdivia28 <felipepedro@aluno.senai.br>
2024-09-23 10:52:42 -0300

    refactor: Rename methods in CampeonatoRepository
    This commit refactors the methods in the `CampeonatoRepository` class to follow consistent naming conventions. The `getCampeonato` method is renamed to `getCampeonatos` to reflect that it retrieves all championships. The `createcampeonato` method is renamed to `createCampeonato` to use camel case. The `updatecampeonato` method is removed as it is not used. These changes improve code readability and maintainability.

--- src/models/campeonato/campeonatoRepository.js
+++ src/models/campeonato/campeonatoRepository.js
@@ -3,37 +3,33 @@ export default class UsersRepository {
   constructor() {
     this.pg = pg;
   };
-
-  async getCampeonato() {
+  async getCampeonatos() {
     try {
-      const allCampeonato = await this.pg.manyOrNone("SELECT * FROM Campeonato");
-      return allCampeonato
-    }
-    catch (error) {
+      const allCampeonatos = await this.pg.manyOrNone("SELECT * FROM Campeonato");
+      return allCampeonatos;
+    } catch (error) {
+      throw error;
+    } 
+   }
+  async getCampeonatoById(id) {
+    try {
+      const campeonato = await this.pg.oneOrNone("SELECT * FROM Campeonato WHERE id = $1", id);
+      return campeonato;
+    } catch (error) {
       throw error;
     }
-    }
-    async getCampeonatoById(id) {
+  }
+  async createCampeonato(campeonato) {
     try {
-        const campeonato = await this.pg.oneOrNone("SELECT * FROM Campeonato WHERE id = $1", id);
-        return campeonato;
-        }
-        catch (error) {
-        throw error;
-        }
+      await this.pg.none("INSERT INTO Campeonato (id, titulo, data_inicio, data_final) VALUES ($1, $2, $3, $4)", [
+        campeonato.id,
+        campeonato.titulo,
+        campeonato.data_inicio,
+        campeonato.data_final
+      ]);
+      return campeonato;
+    } catch (error) {
+      throw error;
     }
-    async createcampeonato(campeonato) {
-    try {
-        await this.pg.none("INSERT INTO Campeonato (id, titulo, data_inicio, data_final) VALUES ($1, $2, $3, $4)", [
-            campeonato.id,
-            campeonato.titulo,
-            campeonato.data_inicio,
-            campeonato.data_final
-        ]);
-        return campeonato;
-        }
-        catch (error) {
-        throw error;
-        }
-    }
-    async updatecampeonato(id, titulo, data_inicio, data_final) {
+  }
+  
