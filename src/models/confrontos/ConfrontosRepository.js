ecced25b9abe5567802fdf8e841a2642a73ff968
joaosantos564 <joaosantos@aluno.senai.br>
2024-10-07 11:02:46 -0300

    confronto id

--- src/models/confrontos/ConfrontosRepository.js
+++ src/models/confrontos/ConfrontosRepository.js
@@ -32,7 +32,7 @@ export default class ConfrontosRepository {
     async getPartidaByConfrontos() {
         try {
             const PartidaPorConfronto = await this.pg.manyOrNone(
-                "SELECT partida.id , partida.data, partida.anotacao FROM confronto INNER JOIN partida ON partida.id = confronto.idPartida"
+                "SELECT confronto.id, partida.id , partida.data, partida.anotacao FROM confronto INNER JOIN partida ON partida.id = confronto.idPartida"
             )
         } catch (error) {
             throw error;
