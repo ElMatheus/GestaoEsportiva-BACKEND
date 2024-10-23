544af4d21e93ae6847a910872127285f5b2c23ee
ElMatheus <matheus.goncalves5@aluno.senai.br>
2024-10-23 11:53:15 -0300

    Refactor createConfronto function and remove unnecessary comments

--- src/controllers/gerar.controller.js
+++ src/controllers/gerar.controller.js
@@ -10,7 +10,7 @@ const confrontosRepository = new ConfrontoRepository();
 
 export const gerarTodosConfrontos = async (req, res) => {
     try {
-        const { data, anotacao, updateUser, modalidade_id, tipoModalidade } = req.body;
+        const { data, anotacao, updateUser, modalidade_id } = req.body;
 
         // 1. Criar uma nova partida e obter seu ID
         const partida = new Partida(data, anotacao, updateUser);
@@ -20,53 +20,21 @@ export const gerarTodosConfrontos = async (req, res) => {
         // 2. Buscar todos os times disponíveis
         const times = await timesRepository.getTimeByModalidadeID(modalidade_id);
 
-        let confrontos = [];
+        // 3. Gerar confrontos todos contra todos com dados dos times
+        const confrontos = [];
+        for (let i = 0; i < times.length; i++) {
+            for (let j = i + 1; j < times.length; j++) {
+                const timeA = times[i];
+                const timeB = times[j];
 
-        if (tipoModalidade === "contra todos") {
-            // 3. Gerar confrontos todos contra todos com dados dos times
-            for (let i = 0; i < times.length; i++) {
-                for (let j = i + 1; j < times.length; j++) {
-                    const timeA = times[i];
-                    const timeB = times[j];
-
-                    // Adicionar os dados dos times nos confrontos
-                    confrontos.push({
-                        id: uuidv4(),
-                        idPartida: partidaId,
-                        timeA: { id: timeA.id, nome: timeA.nome }, // Dados do time A
-                        timeB: { id: timeB.id, nome: timeB.nome }, // Dados do time B
-                        winner: false,
-                        tie: false
-                    });
-                }
-            }
-        } else {
-            // Gerar apenas dois confrontos em uma partida, todos os times jogam em partidas separadas
-            for (let i = 0; i < times.length; i += 2) {
-                if (i + 1 < times.length) {
-                    const timeA = times[i];
-                    const timeB = times[i + 1];
-
-                    confrontos.push({
-                        id: uuidv4(),
-                        idPartida: partidaId,
-                        timeA: { id: timeA.id, nome: timeA.nome }, // Dados do time A
-                        timeB: { id: timeB.id, nome: timeB.nome }, // Dados do time B
-                        winner: false,
-                        tie: false
-                    });
-                } else {
-                    const timeA = times[i];
-
-                    confrontos.push({
-                        id: uuidv4(),
-                        idPartida: partidaId,
-                        timeA: { id: timeA.id, nome: timeA.nome }, // Dados do time A
-                        timeB: null, // Sem adversário
-                        winner: false,
-                        tie: false
-                    });
-                }
+                // Adicionar os dados dos times nos confrontos
+                confrontos.push({
+                    id: uuidv4(),
+                    idPartida: partidaId,
+                    timeA: { partidaId ,id: timeA.id, nome: timeA.nome }, // Dados do time A
+                    timeB: { partidaId ,id: timeB.id, nome: timeB.nome }, // Dados do time B
+                    data: '2024-11-01',
+                });
             }
         }
 
@@ -76,18 +44,18 @@ export const gerarTodosConfrontos = async (req, res) => {
                 id: confronto.id,
                 idPartida: confronto.idPartida,
                 timeId: confronto.timeA.id,
+                data: confronto.data,
                 winner: confronto.winner,
                 tie: confronto.tie
             });
-            if (confronto.timeB) {
-                await confrontosRepository.createConfronto({
-                    id: uuidv4(),
-                    idPartida: confronto.idPartida,
-                    timeId: confronto.timeB.id,
-                    winner: confronto.winner,
-                    tie: confronto.tie
-                });
-            }
+            await confrontosRepository.createConfronto({
+                id: uuidv4(),
+                idPartida: confronto.idPartida,
+                timeId: confronto.timeB.id,
+                data: confronto.data,
+                winner: confronto.winner,
+                tie: confronto.tie
+            });
         }
 
         return res.status(201).send({
