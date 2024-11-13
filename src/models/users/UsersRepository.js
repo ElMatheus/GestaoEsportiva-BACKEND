859ec0c43e29cac97ec604d01f2121fba23236b0
Matheus Henrique de Carvalho Côcco <matths.henrique@yahoo.com>
2024-11-13 17:09:38 -0300

    fix: corrigir consulta de usuário para usar nome sem transformação para minúsculas

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -57,7 +57,7 @@ export default class UsersRepository {
 
   async getUserByName(nome) {
     try {
-      const user = await this.pg.oneOrNone("SELECT * FROM users WHERE LOWER(nome) LIKE $1", nome.toLowerCase());
+      const user = await this.pg.oneOrNone("SELECT * FROM users WHERE LOWER(nome) LIKE $1", nome);
       return user;
     } catch (error) {
       throw error;
