438034a0fd01525d96a587e5a32768139b2f5484
Matheus Henrique de Carvalho Côcco <matths.henrique@yahoo.com>
2024-11-27 16:59:39 -0300

    fix: add authentication middleware for admin access to gerarTodosConfrontos route

--- src/routes/gerar.routes.js
+++ src/routes/gerar.routes.js
@@ -3,9 +3,10 @@ import { Router } from "express";
 import {
     gerarTodosConfrontos
 } from "../controllers/gerar.controller.js";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated.js";
 
 const gerarRouter = Router();
 
-gerarRouter.get("/", gerarTodosConfrontos);
+gerarRouter.get("/", ensureAuthenticated('admin'), gerarTodosConfrontos);
 
 export default gerarRouter;
